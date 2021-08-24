aba19f6fe491583ab18851ed7353e1f6f58dc0b7
MindistCalm <51086034+MIndistCalm@users.noreply.github.com>
2021-08-24 23:49:17 +0700

    mavke valid input

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import CounterCl from './CounterClass'
 import CounterFn from './Counter'
 
 import UserCard from './UserCard'
@@ -11,17 +10,13 @@ const App = () => {
                     Header
                 </header>
                 <div>
-                    <h2>Test Class</h2>
-                    <CounterCl/>
                     <hr/>
-                    <h2>Test Class</h2>
+                    <h2>Test function</h2>
                     <CounterFn min={1} max={10} />
                     <hr/>
                 </div>
                 <footer>
                     footer
-                    <UserCard name="Olga" description="With Apple"/>
-                    <UserCard name="Olga" description="With Banana"/>
                 </footer>
             </div>
 }
